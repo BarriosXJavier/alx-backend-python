526ea7c6ef16d93359038410f4ea49eefcc38d69
David Muriithi <muriithid05@gmail.com>
2024-07-09 10:12:00 +0300

    Refactor task_wait_n function to use task_wait_random for improved readability and maintainability

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """
-    a function that takes a int max_delay and returns a asyncio.Task
-    using a regular function syntax
+    altering wait_n into a new function task_wait_n
+    that can be called with the proper arguments.
 """
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-        returns a asyncio.Task
+        task_wait_random n times
     """
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
 
-    return (asyncio.create_task(wait_random(max_delay)))
+    return sorted(wait_times)
