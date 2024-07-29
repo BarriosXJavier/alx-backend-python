c701b7e4a3907631e8c96597ed4b2d6b7f187645
David Muriithi <muriithid05@gmail.com>
2024-07-30 00:48:25 +0300

    Refactor GithubOrgClient init method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" 
-    A github org client
+"""A github org client
 """
 from typing import (
     List,
@@ -20,7 +19,7 @@ class GithubOrgClient:
     ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
-        """Init method of GithubOrg Client"""
+        """Init method of GithubOrgClient"""
         self._org_name = org_name
 
     @memoize
